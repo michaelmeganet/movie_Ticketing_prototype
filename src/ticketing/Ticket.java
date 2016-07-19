08710f990e94134549848a3d6d36154f0d68b0da
cct3000 <meganet001@gmail.com>
2016-07-19 15:51:47 +0800

    firat commit

---
+++ src/ticketing/Ticket.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ticketing;
+import java.awt.*;
+//import javax.swing.*;
+import java.awt.event.*;
+import java.util.*;
+import java.io.*;
+/**
+ *
+ * @author Administrator
+ */
+public final  class Ticket {
+       static int getSeatPrice (int Input, String x){
+        return 0;
+       }
+     
+        static String calculateStringTotal(int Value){
+            String Y ="Hello";
+            return Y;
+        }
+    }
+    
+           
+
